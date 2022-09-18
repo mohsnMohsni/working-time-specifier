9157f035cb9fa6a98e36643c8aed3887f1d973fa
Mohsen Mohseni <mohsenmohseni.co@gmail.com>
2022-09-18 12:13:47 +0430

    feat: read from conf json file to specify wich branch should init

--- src/core/specifier.py
+++ src/core/specifier.py
@@ -1,3 +1,4 @@
+import json
 from typing import Generator
 from random import randint, shuffle
 
@@ -7,25 +8,45 @@ from openpyxl import load_workbook
 from .._calendar.manager import WorkingsDate
 
 
-path = "تخصیص.xlsx"
 set_locale('fa_IR')
 
 
-def generate_random_specifie_time(sum_of_random: int = 8) -> list:
+def generate_random_specifie_time(branches_col: int = 10, sum_of_random: int = 8) -> list:
     '''
     Generate list of randoms number that sum of them is specified in range.
     '''
 
     num_sum, numbers = int(), list()
-    for index in range(1, 10):
+    for index in range(1, branches_col):
         n = randint(0, sum_of_random - num_sum)
         numbers.append(n)
         num_sum += n
         if num_sum == sum_of_random:
-            numbers += [0] * (10 - index)
+            numbers += [0] * (branches_col - index)
             break
     shuffle(numbers)
+    print(numbers)
     return numbers
+
+
+def work_branch_handler(conf: dict) -> list:
+    '''
+    Read from conf to specify generated times should init into which branch of work.
+    '''
+
+    _times = generate_random_specifie_time(sum(conf.values()) * 2)
+    return {
+        6: _times.pop() if conf['mainSite'] else 0,
+        7: _times.pop() if conf['mainSite'] else 0,
+        8: _times.pop() if conf['mag'] else 0,
+        9: _times.pop() if conf['mag'] else 0,
+        10: _times.pop() if conf['managementPanel'] else 0,
+        11: _times.pop() if conf['managementPanel'] else 0,
+        12: _times.pop() if conf['workmanPanel'] else 0,
+        13: _times.pop() if conf['workmanPanel'] else 0,
+        14: _times.pop() if conf['clientPanel'] else 0,
+        15: _times.pop() if conf['clientPanel'] else 0,
+    }
 
 
 class XlsxHandler:
@@ -34,7 +55,11 @@ class XlsxHandler:
     '''
 
     def __init__(self) -> None:
-        self.wb_obj = load_workbook(path)
+        with open('config.json', 'r') as conf_file:
+            conf = json.load(conf_file)
+            self.xlsPath = conf.pop('xlsPath')
+            self.conf = conf
+        self.wb_obj = load_workbook(self.xlsPath)
         self.sheet_obj = self.wb_obj.active
         self.r_data = self.retrive_last_row_data()
 
@@ -43,7 +68,7 @@ class XlsxHandler:
         return {i: cell.value for i, cell in enumerate(one_left_to_last_row)}
 
     def retrive_rows_to_be_filled(self) -> set:
-        date_inserted_before  = j_date(*map(int, self.r_data.get(5).split('.')))
+        date_inserted_before = j_date(*map(int, self.r_data.get(5).split('.')))
         return WorkingsDate.retrive_working_dates(date_inserted_before.togregorian())
 
     def generate_rows_data(self) -> Generator:
@@ -58,7 +83,7 @@ class XlsxHandler:
                 0: index,
                 4: r_jalalit_date.jweekday(),
                 5: r_jalalit_date.strftime("%Y.%m.%d"),
-                **dict(enumerate(generate_random_specifie_time(), 6))
+                **dict(enumerate(work_branch_handler(self.conf), 6)),
             }
 
     def init_xls(self) -> None:
@@ -69,4 +94,4 @@ class XlsxHandler:
             for j, cell_value in enumerate(row.values(), 1):
                 ws_cell = self.sheet_obj.cell(row=index + i, column=j)
                 ws_cell.value = cell_value
-        self.wb_obj.save(path)
+        self.wb_obj.save(self.xlsPath)
